72335c1b5dfac4b38ebf86c261eff8969c017fde
Leonov Artem <leonovru@yandex.ru>
2019-01-22 17:25:09 +0300

    Add interface ISorting.cs and class Sorting.cs

---
+++ MergeSorting/Sorting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSorting
+{
+    class Sorting<T> : ISorting<T> where T : IComparable
+    {
+
+    }
+}
