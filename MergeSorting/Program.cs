fc33fddcf23f570ef5c442017f875a03f5d2b264
Leonov Artem <leonovru@yandex.ru>
2019-01-22 18:14:44 +0300

    Add method Merge() and add test in Program.cs

--- MergeSorting/Program.cs
+++ MergeSorting/Program.cs
@@ -10,6 +10,13 @@ namespace MergeSorting
     {
         static void Main(string[] args)
         {
+            Sorting<int> sorting = new Sorting<int>(new int[] { 6, 3, 7, 12, 68, 21, -4 });
+            sorting.Print();
+
+            sorting.MergeSort();
+            sorting.Print();
+
+            Console.ReadKey();
         }
     }
 }
--- MergeSorting/SortingAddition.cs
+++ MergeSorting/SortingAddition.cs
@@ -15,11 +15,30 @@ namespace MergeSorting
             int mid = (lower_bound + upper_bound) / 2;
             RecMergeSort(temp, lower_bound, mid);
             RecMergeSort(temp, mid + 1, upper_bound);
-            Merge(temp, lower_bound, mid, upper_bound);
+            Merge(temp, lower_bound, mid + 1, upper_bound);
         }
-        private void Merge(T[] temp, int lower_bound, int mid, int upper_bound)
+        private void Merge(T[] temp, int lower_limiter, int high_limiter, int upper_bound)
         {
+            int i = 0;
+            int lower_bound = lower_limiter;
+            int mid = high_limiter - 1;
 
+            while(lower_limiter <= mid && high_limiter <= upper_bound)
+            {
+                if (array[lower_limiter].CompareTo(array[high_limiter]) < 0)
+                    temp[i++] = array[lower_limiter++];
+                else
+                    temp[i++] = array[high_limiter++];
+            }
+
+            while (lower_limiter <= mid)
+                temp[i++] = array[lower_limiter++];
+
+            while (high_limiter <= upper_bound)
+                temp[i++] = array[high_limiter++];
+
+            for (i = 0; i < upper_bound - lower_bound + 1; i++)
+                array[lower_bound + i] = temp[i];
         }
     }
 }
